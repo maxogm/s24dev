a4ac7036a3e2b2d966b4eab8040429c39ed6b7aa
Vlade Maksimovic <maxogm@gmail.com>
2014-11-02 15:47:03 +0100

    remove title colour

--- SCB/app/src/main/java/rs/in/jmax/scb/ItemListActivity.java
+++ SCB/app/src/main/java/rs/in/jmax/scb/ItemListActivity.java
@@ -30,8 +30,8 @@ public class ItemListActivity extends Activity implements ItemListFragment.Callb
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_actions, menu);
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
